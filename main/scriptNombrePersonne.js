3178f8f19b1e43e87e50fafdd2757b4a531f75fb
ClementRollin <114920146+ClementRollin@users.noreply.github.com>
2023-02-21 11:34:31 +0100

    Add files via upload

--- main/scriptNombrePersonne.js
+++ main/scriptNombrePersonne.js
@@ -23,3 +23,18 @@ const listItems = document.querySelectorAll('#mobile-list li');
       arrow.style.display = 'block';
     });
 });
+
+
+const tablePerso = document.querySelector("#mobile-list li[value='custom']");
+
+tablePerso.addEventListener("click", function() {
+  let nombre = prompt("Entrez le nombre de place souhaité :");
+  if (nombre > 5) {
+    const nouvelElement = document.createElement("li");
+    nouvelElement.className = "list-item place perso";
+    nouvelElement.textContent = nombre + " places";
+    tablePerso.parentNode.replaceChild(nouvelElement, tablePerso);
+  } else {
+    alert("Le nombre entré est inférieur ou égal à 5. Veuillez entrer un nombre supérieur à 5.");
+  }
+});
